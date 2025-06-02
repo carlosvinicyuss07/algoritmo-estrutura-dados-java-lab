3aee89b300119b9fa6015e7c504babd8a993cf7a
Carlos Vinícyus <carlosvinicyuss@gmail.com>
2025-06-02 14:48:04 -0300

    Melhoria no algoritmo de Fibonacci Recursivo com Memorização

--- src/main/java/algoritmos/recursivos/Fibonacci.java
+++ src/main/java/algoritmos/recursivos/Fibonacci.java
@@ -1,6 +1,8 @@
 package algoritmos.recursivos;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class Fibonacci {
@@ -13,17 +15,16 @@ public class Fibonacci {
         }
     }
 
-    private static Map<Integer, Integer> cache = new HashMap<>();
+    static int[] cache;
     public static int fibonacciRecursivoMemorizado(int n) {
         if (n <= 1) {
             return n;
         }
-        if (cache.containsKey(n)) {
-            return cache.get(n);
+        if (cache[n] != -1) {
+            return cache[n];
         }
-        int result = fibonacciRecursivoMemorizado(n - 1) + fibonacciRecursivoMemorizado(n - 2);
-        cache.put(n, result);
-        return result;
+        cache[n] = fibonacciRecursivoMemorizado(n - 1) + fibonacciRecursivoMemorizado(n - 2);
+        return cache[n];
     }
 
     public static int fibonacciIterativo(int n) {
@@ -43,7 +44,7 @@ public class Fibonacci {
     }
 
     public static void main(String[] args) {
-        int n = 40;
+        int n = 30;
 
         //Medição do tempo para versão recursiva
         long inicioRecursivo = System.nanoTime();
@@ -56,6 +57,11 @@ public class Fibonacci {
         int resultadoIterativo = fibonacciIterativo(n);
         long fimIterativo = System.nanoTime();
         long tempoIterativo = fimIterativo - inicioIterativo;
+
+        cache = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            cache[i] = -1;
+        }
 
         //Medição do tempo para versão recursiva memorizada
         long inicioRecursivoMemorizado = System.nanoTime();
---
+++ src/main/java/estruturas/lineares/estaticas/vetor/TDAVetor.java
@@ -0,0 +1,4 @@
+package estruturas.lineares.estaticas.vetor;
+
+public interface TDAVetor {
+}
